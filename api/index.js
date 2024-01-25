6b968aedfdc0d2e097d3974611f1cd39247a81f9
zikran69 <zikzigi69@gmail.com>
2024-01-26 03:14:27 +0700

    design today's tab

--- api/index.js
+++ api/index.js
@@ -44,3 +44,35 @@ app.post("/habits", async (req, res) => {
     res.status(500).json({ error: "Something went wrong" });
   }
 });
+
+//fetch data from backend
+
+app.get("/habitslist", async (req, res) => {
+  try {
+    const allHabits = await Habit.find({});
+
+    res.status(200).json(allHabits);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/habits/:habitId/completed/:day", async (req, res) => {
+  try {
+    const { habitId, day } = req.params;
+    const habit = await Habit.findById(habitId);
+
+    if (!habit) {
+      return res.status(404).json({ error: "Habit not found" });
+    }
+
+    habit.completed[day] = true;
+
+    await habit.save();
+
+    res.status(200).json({ message: "Habit completed status updated" });
+  } catch (error) {
+    console.log("Error", error);
+    res.status(500).json({ error: error.message });
+  }
+});
--- app/home/index.js
+++ app/home/index.js
@@ -1,12 +1,35 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
-import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  Image,
+} from "react-native";
+import React, { useEffect, useState } from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
+import axios from "axios";
 
 const index = () => {
   const [option, setOption] = useState("Today");
   const router = useRouter();
+  const [habits, setHabits] = useState([]);
+  useEffect(() => {
+    fetchHabits();
+  }, []);
+
+  const fetchHabits = async () => {
+    try {
+      const response = await axios.get("http://192.168.100.14:3000/habitslist");
+      setHabits(response.data);
+    } catch (error) {
+      console.log("error fetching habits", error);
+    }
+  };
+  console.log("habits", habits);
+
   return (
     <ScrollView style={styles.ScrollView}>
       <View style={styles.uperNav}>
@@ -69,6 +92,83 @@ const index = () => {
           </Text>
         </Pressable>
       </View>
+
+      {option == "Today" && habits?.length > 0 ? (
+        <View>
+          {habits?.map((item, index) => (
+            <Pressable
+              key={index}
+              style={{
+                marginVertical: 10,
+                backgroundColor: item?.color,
+                padding: 12,
+                borderRadius: 24,
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontWeight: "500",
+                  color: "white",
+                }}
+              >
+                {item?.title}
+              </Text>
+            </Pressable>
+          ))}
+        </View>
+      ) : (
+        <View
+          style={{
+            marginTop: 150,
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: "auto",
+          }}
+        >
+          <Image
+            style={{ width: 60, height: 60, resizeMode: "cover" }}
+            source={{
+              uri: "https://www.svgrepo.com/show/489659/empty-box.svg",
+            }}
+          />
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 20,
+              fontWeight: "600",
+              marginTop: 10,
+            }}
+          >
+            No Habits for today
+          </Text>
+
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 20,
+              fontWeight: "600",
+              marginTop: 10,
+            }}
+          >
+            No Habits for today.Create one?
+          </Text>
+
+          <Pressable
+            onPress={() => router.push("/home/create")}
+            styles={{
+              backgroundColor: "#0071c5",
+              marginTop: 20,
+              paddingHorizontal: 20,
+              paddingVertical: 10,
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          >
+            <Text>Create</Text>
+          </Pressable>
+        </View>
+      )}
     </ScrollView>
   );
 };
